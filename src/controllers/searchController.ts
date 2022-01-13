a09f86997bed62ad470d0d83053bed32be74ec1c
Quenede Abreu <quenede.in@gmail.com>
2022-01-13 17:42:13 -0500

    Deploy heroku

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { createMenuObject } from '../helpers/createMenuObject';
-import { Pet } from '../models/pet';
+import { Pet } from '../models/Pet';
 
 export const search = (req: Request, res: Response) => {
   let namePet: string = req.query.q as string;
